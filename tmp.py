f93117a06b4ae0104a2108ba037b20f02f17a702
misteltein <itogahibiki@gmail.com>
2020-12-11 18:36:04 +0900

    wip

--- tmp.py
+++ tmp.py
@@ -4,23 +4,46 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-data = np.loadtxt('sample.csv',delimiter=',')
-data_train, data_test = train_test_split(data,test_size=0.2,random_state=0)
-y_train = data_train[:,2]
-X_train = data_train[:,:2]
-y_test = data_test[:,2]
-X_test = data_test[:,:2]
+data = np.loadtxt('sample2.csv',delimiter=',')
 
-model = svm.SVC(kernel='linear',C=1.0)
-model.fit(X_train,y_train)
+cs = np.logspace(-2,10,13,base=10)
+gs = np.logspace(-9,3,13,base=10)
 
-pred_test = model.predict(X_test)
-accuracy_train = accuracy_score(y_test, pred_test)
-print('推定の正解率： %.2f' % accuracy_train)
+scores = {}
 
-#fig = plt.figure()
-#ax = fig.add_subplot()
-#ax.set_xlabel('$x_1$')
-#ax.set_ylabel('$x_2$')
-#plt.scatter(X[:,0],X[:,1],c=y,s=1)
-#plt.show()
+for rs in range(0,1,1):
+    data_train, data_test = train_test_split(data,test_size=0.25,random_state=rs)
+    y_train = data_train[:,2]
+    X_train = data_train[:,:2]
+    y_test = data_test[:,2]
+    X_test = data_test[:,:2]
+
+    for g in gs:
+        for c in cs:
+            model = svm.SVC(C=c,gamma=g)
+            model.fit(X_train,y_train)
+            pred_test = model.predict(X_test)
+            accuracy_train = accuracy_score(y_test, pred_test)
+            print(g, c, accuracy_train )
+            if (g,c) in scores:
+                scores[(g,c)] += accuracy_train
+            else:
+                scores[(g,c)]=accuracy_train
+
+X1 = []
+X2 = []
+A = []
+for k in scores.keys():
+    X1.append(k[0])
+    X2.append(k[1])
+    A.append(scores[k]/(len(cs)*len(gs)))
+
+
+import matplotlib.cm as cm
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.set_xscale('log')
+ax.set_yscale('log')
+sc = ax.scatter(X1,X2,c=A,cmap=cm.coolwarm)
+plt.colorbar(sc)
+plt.show()
