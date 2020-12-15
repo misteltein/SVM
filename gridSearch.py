151f68ff06c529aae92b5261341314bb8962a40e
misteltein <itogahibiki@gmail.com>
2020-12-15 23:21:38 +0900

    Added sc

--- gridSearch.py
+++ gridSearch.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 from sklearn import svm
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score
@@ -7,7 +8,6 @@ from mlxtend.plotting import plot_decision_regions
 
 sample_name = 'sampleA'
 data = np.loadtxt(sample_name + '.csv', delimiter=',')
-
 
 params = [
     {'C': np.logspace(0, 4, 9, base=10), 'kernel': ['linear']}
@@ -19,9 +19,12 @@ gscv = GridSearchCV(svm.SVC(), params, cv=10, verbose=3,
 data_train, data_test = train_test_split(data, test_size=0.25, random_state=0)
 
 y_train = data_train[:, 2].astype(int)
-X_train = data_train[:, :2]
 y_test = data_test[:, 2].astype(int)
-X_test = data_test[:, :2]
+
+sc = preprocessing.StandardScaler().fit(data_train[:, :2])
+
+X_train = sc.transform(data_train[:, :2])
+X_test = sc.transform(data_test[:, :2])
 
 gscv.fit(X_train, y_train)
 
