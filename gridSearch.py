0120194cdf272006340e6a215d340afdbd522d64
misteltein <itogahibiki@gmail.com>
2020-12-15 10:29:28 +0900

    add

---
+++ gridSearch.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm 
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score
+from mlxtend.plotting import plot_decision_regions
+
+sample_name = 'sampleA'
+data = np.loadtxt(sample_name+'.csv',delimiter=',')
+
+
+params = [
+        {'C': np.logspace(0,4,9,base=10), 'kernel':['linear']} 
+    ]
+
+gscv = GridSearchCV(svm.SVC(),params,cv=10, verbose=3, scoring='accuracy', n_jobs=8 )
+
+data_train, data_test = train_test_split(data,test_size=0.25,random_state=0)
+
+y_train = data_train[:,2].astype(int)
+X_train = data_train[:,:2]
+y_test = data_test[:,2].astype(int)
+X_test = data_test[:,:2]
+
+gscv.fit(X_train,y_train)
+y_pred = gscv.predict(X_test)
+
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.legend(loc='upper right')
+ax.set_title('(a)')
+ax.scatter(X_train[:,0],X_train[:,1],c=y_train,s=2)
+ax.legend().remove()
+plt.savefig(sample_name+'_a.png',dpi=300)
+plt.show()
+plt.close()
+
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.set_title('(b)')
+plot_decision_regions(X_train,y_train, clf=gscv,  res=0.01)
+plt.savefig(sample_name+'_b.png',dpi=300)
+plt.show()
+plt.close()
+
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.set_title('(c)')
+plot_decision_regions(X_test, y_test, clf=gscv,  res=0.01)
+plt.savefig(sample_name+'_c.png',dpi=300)
+plt.show()
+plt.close()
+
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.set_title('(d)')
+plot_decision_regions(X_test, y_pred, clf=gscv,  res=0.01)
+plt.savefig(sample_name+'_d.png',dpi=300)
+plt.show()
+plt.close()
+
+
+print('\nResult:')
+print('\tBest Score: ', gscv.best_score_)
+print('\tBest Params: ', gscv.best_params_ )
+print('\tAccuracy (test): ',accuracy_score(y_test,y_pred))
+
--- tmp2.py
+++ tmp2.py
@@ -2,21 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm 
 from sklearn.model_selection import train_test_split, GridSearchCV
-#from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
 from mlxtend.plotting import plot_decision_regions
 
 run_name = 'hoo'
-data = np.loadtxt('sample22.csv',delimiter=',')
+data = np.loadtxt('sample1.csv',delimiter=',')
 
 params = [
-        #{'C': np.logspace(-2,4,7,base=10), 'kernel':['linear']}#,
+        {'C': np.logspace(-2,4,7,base=10), 'kernel':['linear']}#,
         #{'C': np.logspace(-2,4,1000,base=10), 'kernel':['linear']}#,
-        {'C': np.linspace(473,475,10), 'kernel':['linear']}#,
+        #{'C': np.linspace(473,475,10), 'kernel':['linear']}#,
         #{'C': np.logspace(-2,4,13,base=10), 'gamma':np.logspace(-5,1,13,base=10), 'kernel':['rbf']},
         #{'C': np.logspace(-2,4,13,base=10), 'degree':[2,3], 'gamma': np.logspace(-5,1,13,base=10), 'kernel':['poly']}
         #{'C': np.linspace(14000,16000,10), 'degree':[2], 'gamma': np.linspace(1.0,1.5,10), 'kernel':['poly']}
         #{'C': np.logspace(-2,4,7,base=10), 'degree':[3], 'gamma': np.logspace(-5,1,7,base=10), 'kernel':['poly']}
+        #{'C': np.logspace(-2,4,7,base=10), 'degree':[2], 'gamma': np.logspace(-5,1,7,base=10), 'coef0':np.linspace(-10,10,10),'kernel':['poly']}
         #{'C': np.logspace(3,4,1000,base=10), 'gamma':np.logspace(-3,-2,1000,base=10), 'kernel':['rbf']}#,
         #{'C': np.linspace(4150,4200,100), 'gamma':np.linspace(0.0090,0.0091,100), 'kernel':['rbf']}#,
     ]
@@ -31,18 +31,12 @@ y_test = data_test[:,2].astype(int)
 X_test = data_test[:,:2]
 
 gscv.fit(X_train,y_train)
-
-print( 'best score: ', gscv.best_score_)
-print( 'best params: ', gscv.best_params_ )
 y_pred = gscv.predict(X_test)
-print('test:')
-print(classification_report(y_test, y_pred))
+
 
 fig = plt.figure()
 ax = fig.add_subplot()
 ax.legend(loc='upper right')
-ax.set_xlabel('$x_1$')
-ax.set_ylabel('$x_2$')
 ax.set_title('(a)')
 ax.scatter(X_train[:,0],X_train[:,1],c=y_train,s=2)
 ax.legend().remove()
@@ -51,8 +45,6 @@ plt.close()
 
 fig = plt.figure()
 ax = fig.add_subplot()
-ax.set_xlabel('$x_1$')
-ax.set_ylabel('$x_2$')
 ax.set_title('(b)')
 plot_decision_regions(X_train,y_train, clf=gscv,  res=0.01)
 plt.savefig(run_name+'_b.png',dpi=300)
@@ -60,8 +52,6 @@ plt.close()
 
 fig = plt.figure()
 ax = fig.add_subplot()
-ax.set_xlabel('$x_1$')
-ax.set_ylabel('$x_2$')
 ax.set_title('(c)')
 plot_decision_regions(X_test, y_test, clf=gscv,  res=0.01)
 plt.savefig(run_name+'_c.png',dpi=300)
@@ -69,10 +59,14 @@ plt.close()
 
 fig = plt.figure()
 ax = fig.add_subplot()
-ax.set_xlabel('$x_1$')
-ax.set_ylabel('$x_2$')
 ax.set_title('(d)')
 plot_decision_regions(X_test, y_pred, clf=gscv,  res=0.01)
 plt.savefig(run_name+'_d.png',dpi=300)
 plt.close()
+
+
+print('\nResult:')
+print('\tBest Score: ', gscv.best_score_)
+print('\tBest Params: ', gscv.best_params_ )
+print('\tAccuracy(test):',accuracy_score(y_test,y_pred))
 
